93489ca8051f9c4be17adff7226568aef697d9f8
Aditya <145000575+Aditya19110@users.noreply.github.com>
2025-06-08 17:04:47 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import { Navbar } from './components/Navbar';
 import { MobileMenu } from './components/MobileMenu';
 import { Home } from './components/sections/Home';
 import { About } from './components/sections/About';
-import { Projects } from './components/sections/projects';
+import { Projects } from './components/sections/Projects';
 import { Contact } from './components/sections/Contact';
 
 function App() {
